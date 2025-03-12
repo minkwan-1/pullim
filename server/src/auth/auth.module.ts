7e5ac4c01c8522a610da0f41f7a97729703eea5e
원민관 <mkwan5741@gmail.com>
2025-03-12 20:08:27 +0900

    create auth module

---
+++ server/src/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [],
+  providers: [],
+  exports: [],
+})
+export class AuthModule {}
